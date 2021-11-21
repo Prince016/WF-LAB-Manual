49107d1fc41381d12c5aa3253efcd0d1c5cd49a0
Prashant <70711348+Prince016@users.noreply.github.com>
2021-11-21 18:21:03 +0530

    Experiment -8

---
+++ experiment-8/exp_8.js
@@ -0,0 +1,15 @@
+function logPoint(point) {
+    console.log("x = " + point.x + ", y = " + point.y);
+}
+function logName(x) {
+    console.log("Hello, " + x.name);
+}
+var obj = {
+    x: 0,
+    y: 0,
+    name: "Origin"
+};
+logPoint(obj);
+logName(obj);
+// --------------------------------------
+//   -----------------------------------------------------
---
+++ experiment-8/second.ts
@@ -0,0 +1,7 @@
+// function fn(arg: Empty) {
+//     // do something?
+//   }
+   
+//   // No error, but this isn't an 'Empty' ?
+//   fn({ k: 10 });
+  
